1eab97494073d8a757dfef80f8b20806bfc58a37
Ruslan Garifullin <buynov0984@gmail.com>
2025-03-16 19:24:54 +0300

    added exception handling

---
+++ TelegramBotMenu/TaskCountLimitException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotMenu
+{
+    internal class TaskCountLimitException : Exception
+    {
+        public TaskCountLimitException(int taskCountLimit) 
+            : base($"Превышено максимальное количество задач, равное {taskCountLimit}") { }
+    }
+}
---
+++ TelegramBotMenu/TaskLengthLimitException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotMenu
+{
+    internal class TaskLengthLimitException : Exception
+    {
+        public TaskLengthLimitException(int taskLength, int taskLengthLimit) 
+            : base($"Длина задачи {taskLength} превышает максимально допустимое значение {taskLengthLimit}") { }
+    }
+}
